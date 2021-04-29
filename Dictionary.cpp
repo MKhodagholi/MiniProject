101d7c011f95ef72e99b6839da68b877c0a2dafc
Mohammad <MKhodaagholi@gmail.com>
2021-04-29 14:39:38 +0430

    [start coding Dictionary.cpp file with (addWord function)]

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,1 +1,15 @@
 #include "Dictionary.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    string str_input;
+    Node *nex;
+};
+
+void Dictionary::addWordWithSynonyms() {
+    string wordWithSynonyms;
+    getline(cin, wordWithSynonyms);
+}
