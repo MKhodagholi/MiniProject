d753a5d2fb28cbee411141fcadfbc9d726fc27d6
Mohammad <MKhodaagholi@gmail.com>
2021-04-29 22:30:36 +0430

    [coding the body of main for func addWord...]

--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <string>
 #include "Dictionary.h"
 
 using namespace std;
 
 int main() {
-    Words *temp = creatWord("book");
-    showMenu();
+
+    Words *Head;
+    Head->nex = nullptr;
+    int num;
+
+    addWordWithSynonyms(Head);
+
+    do{
+        showMenu(); // this func is for showing Menu of Program
+        cout << endl;
+        cin >> num;
+
+        switch (num) {
+            case 1: // Add word with synonyms into Dictionary
+            addWordWithSynonyms(Head);
+                break;
+
+            case 2: // Remove word with synonyms
+                break;
+
+            case 3:
+                break;
+
+            case 4: // Search word with synonyms
+                break;
+
+            case 5: // Show word with synonyms
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
+
+            case 9:
+                break;
+        }
+    } while(num != 9);
 }
