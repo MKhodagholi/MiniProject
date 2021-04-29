b5cb560b0675d9a1512b94f213bae1494d1068e8
Mohammad <MKhodaagholi@gmail.com>
2021-04-29 10:08:00 +0430

    creat a empty project, main.cpp, Dictionary.cpp and .h file

---
+++ Dictionary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mohammad on 4/29/21.
+//
+
+#include "Dictionary.h"
---
+++ Dictionary.h
@@ -0,0 +1,14 @@
+//
+// Created by mohammad on 4/29/21.
+//
+
+#ifndef MINIPROJECT_1400_02_10_DICTIONARY_H
+#define MINIPROJECT_1400_02_10_DICTIONARY_H
+
+
+class Dictionary {
+
+};
+
+
+#endif //MINIPROJECT_1400_02_10_DICTIONARY_H
