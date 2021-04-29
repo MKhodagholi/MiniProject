2e0da46104171fab55a2dc237e2c7eee1d7ac188
Mohammad <MKhodaagholi@gmail.com>
2021-04-29 22:22:10 +0430

    [update addWord... func and creat stringToArray func]

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 
 void showMenu(){
-    cout << "1) Add word with synonyms into Dictionary\n";
-    cout << "2) Remove word with synonyms\n";
-    cout << "3) Remove word's synonyms\n";
-    cout << "4) Search word with synonyms\n";
-    cout << "5) Show word with synonyms\n";
+    cout << "1) Add Word\n";
+    cout << "2) Remove Word\n";
+    cout << "3) Remove Word's synonyms\n";
+    cout << "4) Search\n";
+    cout << "5) Show Word\n";
     cout << "6) change dictation of words\n";
     cout << "7) Save\n";
     cout << "8) Read File\n";
@@ -25,13 +25,48 @@ Words* creatWord(string name){
 }
 
 
-void addWordWithSynonyms(string name, Words *Head) {
+void addWordWithSynonyms(Words *Head) {
+    cout << "Enter the Word: ";
+    string name;
+    cin >> name;
     Words *temp = new Words;
     temp = Head;
-    Words *word = creatWord(name);
+    Words *word = new Words;
+    word = creatWord(name);
     while (temp->nex != nullptr){
         temp = temp->nex;
     }
     temp->nex = word;
+    cout << "How many you want to add synonyms of \"" << name << "\"? ";
+    int num;
+    cin >> num;
+    string synonyms[num];
+    cin >> name;
+    stringToArray(name, synonyms, num);
+    for (int i = 0; i < num; i++){
+        cout << synonyms[i] << endl;
+    }
+/*    for (int i = 0; i < num; i++){
+        string syno;
+        getline(cin, syno);
+        Words *temp1 = new Words;
+        temp1 = creatWord(syno);
+        temp1 = temp1->nex;
+        temp1->syno = NULL;
+        temp1->nex = nullptr;
+    }*/
+}
 
+void stringToArray(string str, string ans[], int size){
+    string str_making;
+    int count = 0;
+    for (int i = 0; i < str.length(); i++){
+        if (str[i] == ' '){
+            count++;
+            str_making = "";
+        }
+        else {
+            ans[count] += str[i];
+        }
+    }
 }
